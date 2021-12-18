410def2d61a9d8b93cf194de5be00b3fd4597edf
zeekee <dev.ezequiel@gmail.com>
2021-12-17 22:17:32 -0300

    desarrollo

---
+++ Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace Encrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.key2Box = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.key1Box = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage3.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(306, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(177, 50);
+            this.title.TabIndex = 1;
+            this.title.Text = "Encrypter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(105, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(576, 42);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "When encrypting, two files will be generated with the keys for decryption.\r\nIf yo" +
+    "u lose these keys, you will never be able to recover the original file.";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.decryptBtn);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.key2Box);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.key1Box);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.listBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(768, 378);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Decrypt";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(648, 331);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(114, 41);
+            this.decryptBtn.TabIndex = 26;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(574, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(574, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Drag and drop Key 2:";
+            // 
+            // key2Box
+            // 
+            this.key2Box.FormattingEnabled = true;
+            this.key2Box.ItemHeight = 15;
+            this.key2Box.Location = new System.Drawing.Point(574, 33);
+            this.key2Box.Name = "key2Box";
+            this.key2Box.Size = new System.Drawing.Size(188, 184);
+            this.key2Box.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(380, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Drag and drop Key 1:";
+            // 
+            // key1Box
+            // 
+            this.key1Box.FormattingEnabled = true;
+            this.key1Box.ItemHeight = 15;
+            this.key1Box.Location = new System.Drawing.Point(380, 33);
+            this.key1Box.Name = "key1Box";
+            this.key1Box.Size = new System.Drawing.Size(188, 184);
+            this.key1Box.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Drag and drop files to encrypt below:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(6, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(368, 184);
+            this.listBox2.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.clearBtn);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.encryptBtn);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 378);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Encrypt";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(7, 224);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 16;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Drag and drop files to encrypt below:";
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(648, 331);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(114, 41);
+            this.encryptBtn.TabIndex = 13;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(6, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(756, 184);
+            this.listBox1.TabIndex = 14;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 130);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 406);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 548);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox key2Box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox key1Box;
+        private System.Windows.Forms.Button decryptBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace Encrypter
+{
+    public partial class Form1 : Form
+    {
+        string[] filesPathToEncryp;
+        string[] filesPathToDecryp;
+        string key1PathToDecryp;
+        string key2PathToDecryp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.AllowDrop = true;
+            listBox1.DragEnter += new DragEventHandler(DragEnter);
+            listBox1.DragDrop += new DragEventHandler(DragDrop);
+
+            listBox2.AllowDrop = true;
+            listBox2.DragEnter += new DragEventHandler(DragEnter);
+            listBox2.DragDrop += new DragEventHandler(DragDrop);
+
+            key1Box.AllowDrop = true;
+            key1Box.DragEnter += new DragEventHandler(DragEnter);
+            key1Box.DragDrop += new DragEventHandler(DragDrop);
+
+            key2Box.AllowDrop = true;
+            key2Box.DragEnter += new DragEventHandler(DragEnter);
+            key2Box.DragDrop += new DragEventHandler(DragDrop);
+        }
+
+        public void DragEnter(Object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        public void DragDrop(Object sender, DragEventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            string[] filesPath = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            switch (listBox.Name)
+            {
+                case "listBox1":
+                    filesPathToEncryp = filesPath;
+                    break;
+                case "listBox2":
+                    filesPathToDecryp = filesPath;
+                    break;
+                case "key1Box":
+                    key1PathToDecryp = filesPath.First();
+                    break;
+                case "key2Box":
+                    key2PathToDecryp = filesPath.First();
+                    break;
+            }
+
+            foreach (string file in filesPath)
+            {
+                listBox.Items.Add(Path.GetFileNameWithoutExtension(file));
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            filesPathToEncryp = Array.Empty<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            filesPathToDecryp = Array.Empty<string>();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            key1Box.Items.Clear();
+            key1PathToDecryp = string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            key2Box.Items.Clear();
+            key2PathToDecryp = string.Empty;
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            if (filesPathToEncryp != null && filesPathToEncryp.Length != 0)
+            {
+                byte[] myRijndaelKey;
+                byte[] myRijndaelIV;
+
+                try
+                {
+                    string fileName;
+                    if (filesPathToEncryp.Length == 1)
+                    {
+                        fileName = Path.Combine(Path.GetDirectoryName(filesPathToEncryp.First()), Path.GetFileNameWithoutExtension(filesPathToEncryp.First()));
+                    }
+                    else
+                    {
+                        fileName = Path.Combine(Path.GetDirectoryName(filesPathToEncryp.First()), "Varios");
+                    }
+
+                    using (Rijndael myRijndael = Rijndael.Create())
+                    {
+                        myRijndaelKey = myRijndael.Key;
+                        myRijndaelIV = myRijndael.IV;
+                    }
+
+                    foreach (string file in filesPathToEncryp)
+                    {
+                        using (Rijndael myRijndael = Rijndael.Create())
+                        {
+                            byte[] encrypted = EncryptStringToBytes(File.ReadAllText(file), myRijndaelKey, myRijndaelIV);
+
+                            File.WriteAllText(file, string.Empty);
+                            FileStream fileEncrypt = new FileStream(file, FileMode.Open);
+                            fileEncrypt.Write(encrypted);
+                            fileEncrypt.Close();
+                        }
+                    }
+
+                    FileStream fileEncryptKey = new FileStream($"{fileName} - Key-1", FileMode.Create);
+                    fileEncryptKey.Write(myRijndaelKey);
+                    fileEncryptKey.Close();
+
+                    FileStream fileEncryptIV = new FileStream($"{fileName} - Key-2", FileMode.Create);
+                    fileEncryptIV.Write(myRijndaelIV);
+                    fileEncryptIV.Close();
+
+                    MessageBox.Show("Encryption Done!", "Success");
+                }
+                catch
+                {
+                    MessageBox.Show("Encryption failed!", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No files to encrypt!", "Error");
+            }
+
+        }
+
+        private void decryptBtn_Click(object sender, EventArgs e)
+        {
+            using (Rijndael myRijndael = Rijndael.Create())
+            {
+                foreach (string file in filesPathToDecryp)
+                {
+                    byte[] cryptFile = File.ReadAllBytes(file);
+                    byte[] RijndaelKey = File.ReadAllBytes(key1PathToDecryp);
+                    byte[] RijndaelIV = File.ReadAllBytes(key2PathToDecryp);
+
+                    string decrypt = DecryptStringFromBytes(cryptFile, RijndaelKey, RijndaelIV);
+
+                    File.WriteAllText(file, decrypt);
+                }
+            }
+
+            MessageBox.Show("Decryption done!", "Success");
+        }
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string plaintext = null;
+
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
